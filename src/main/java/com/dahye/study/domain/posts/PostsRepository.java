46f7b4309aa0c52ce0c49e3e6075b5d677270eb8
DahyeIn <dahye1477@naver.com>
2024-12-05 22:30:58 +0900

    posts 생성 및 테스트코드 작성

---
+++ src/main/java/com/dahye/study/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.dahye.study.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
--- src/test/java/com/dahye/study/web/HelloControllerTest.java
+++ src/test/java/com/dahye/study/web/HelloControllerTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 @ExtendWith(SpringExtension.class)
@@ -27,5 +29,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 http get 요청함
                 .andExpect(status().isOk()) // andExpect는 결과 검증, 상태가 200인것
                 .andExpect(content().string(hello)); // 내용이(return값이) hello냐!
+    }
+
+    @Test
+    public void helloDto_return() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
